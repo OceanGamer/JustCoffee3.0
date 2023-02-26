a042e00849334c5c31d8bcc1adaf287fa643fc35
OceanGamer <100978092+OceanGamer@users.noreply.github.com>
2023-02-26 00:18:49 -0400

    Add files via upload

--- js/indexpage.js
+++ js/indexpage.js
@@ -6,9 +6,9 @@ function iniciarApp() {
                 res.json()
                     .then(function(respond){
                         respond.forEach(block => {
-                            podcastzone.innerHTML += '<a href="podcast.html?watch='+block.id+'" class="titlesPOD separation"><div class="podcastcard"><img src="images/podlogos/'+block.imagen+'" alt="" class="podlogo"><p class="titlesPOD text-center">'+block.nombre+'</p></div></a>';
+                            podcastzone.innerHTML += '<a href="podcast?watch='+block.id+'" class="titlesPOD separation"><div class="podcastcard"><img src="images/podlogos/'+block.imagen+'" alt="" class="podlogo"><p class="titlesPOD text-center">'+block.nombre+'</p></div></a>';
                         });
-                        podcastzone.innerHTML += '<a href="podcast.html" class="titlesPOD separation"><div class="podcastcard"><img src="images/more.png" alt="" class="podlogo"><p class="titlesPOD text-center">Ver Todo</p></div></a>'
+                        podcastzone.innerHTML += '<a href="podcast" class="titlesPOD separation"><div class="podcastcard"><img src="images/more.png" alt="" class="podlogo"><p class="titlesPOD text-center">Ver Todo</p></div></a>'
                     })
             }
         })
